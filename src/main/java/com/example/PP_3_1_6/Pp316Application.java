9d5699e9a70f8ea956ff9bfa2e4ad8cb2a65947f
Dmitry Khakhalev <khakhalev@mail.ru>
2023-10-16 14:48:10 +0300

    Создана модель User

---
+++ src/main/java/com/example/PP_3_1_6/Pp316Application.java
@@ -0,0 +1,13 @@
+package com.example.PP_3_1_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp316Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp316Application.class, args);
+	}
+
+}
